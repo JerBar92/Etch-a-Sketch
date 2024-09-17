6363f589341e06e70bfb54455a13795cf9e7d976
JerBar92 <174113693+JerBar92@users.noreply.github.com>
2024-09-17 16:50:24 +0200

    Add button to change grid size

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const container = document.querySelector(".container");
+const size = document.querySelector(".size");
+
+size?.addEventListener("click", () => {
+  const choice = Number(prompt("Chose a number of square between 16 and 100"));
+  const width = (600 - choice * 2) / choice;
+  const height = (600 - choice * 2) / choice;
+  const nbrSquare = choice * choice;
+  container?.replaceChildren();
+  for (let i = 0; i < nbrSquare; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "grid");
+    newDiv.style.height = `${width}px`;
+    newDiv.style.width = `${height}px`;
+    container?.appendChild(newDiv);
+  }
+});
 
 for (let i = 0; i < 256; i++) {
   const newDiv = document.createElement("div");
